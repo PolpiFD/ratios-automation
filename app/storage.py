ebb393241970a9ccd2aaef7feb1eb740defec598
Polpi <anthotuigr@gmail.com>
2025-07-09 23:08:12 +0200

    Amélioration de la sécurité concernant l'url publique du document, passé à 120sec.

--- app/storage.py
+++ app/storage.py
@@ -31,7 +31,7 @@ async def upload_file (data: bytes, filename: str, mime: str) -> str:
     )
     return blob_client.url
 
-def make_read_sas_url (container: str, blob_name: str, minutes: int = 15) -> str:
+def make_read_sas_url (container: str, blob_name: str, seconds: int = 120) -> str:
     print("make_read_sas_url -> OKKK")
     sas = generate_blob_sas(
         account_name=os.getenv("AZURE_STORAGE_ACCOUNT_NAME"),
@@ -39,6 +39,6 @@ def make_read_sas_url (container: str, blob_name: str, minutes: int = 15) -> str
         blob_name=blob_name,
         account_key=os.getenv("AZURE_STORAGE_ACCOUNT_KEY"),
         permission=BlobSasPermissions(read=True),
-        expiry=datetime.utcnow() + timedelta(minutes=minutes)
+        expiry=datetime.utcnow() + timedelta(seconds=seconds)
     )
     return f"{os.getenv('AZURE_STORAGE_ACCOUNT_URL')}/{container}/{blob_name}?{sas}"
